dc66839fe1673ad3f64bf53c054df0a037d891fb
Sanne Karibo <82738483+SanneEmmanuel@users.noreply.github.com>
2025-05-22 23:51:59 +0100

    Altered main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { BrowserRouter } from "react-router-dom";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import App from './App';
 
-createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <Router>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </Router>
+  </React.StrictMode>
 );
