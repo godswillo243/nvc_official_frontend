bcb0ac6b31741d195f1ee779de388b6d0610a705
Sanne Karibo <82738483+SanneEmmanuel@users.noreply.github.com>
2025-06-09 23:19:09 +0100

    Update mutations.ts

--- src/lib/react-query/mutations.ts
+++ src/lib/react-query/mutations.ts
@@ -1,5 +1,5 @@
 import type { UseMutationResult } from "@tanstack/react-query";
-import { useMutation, useQueryClient} from "@tanstack/react-query";
+import type { useMutation, useQueryClient} from "@tanstack/react-query";
 import {
   login,
   signup,
