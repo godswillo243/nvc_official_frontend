835889847a31720fba3c0ac53b814668f9de431b
Sanne Karibo <82738483+SanneEmmanuel@users.noreply.github.com>
2025-06-18 12:28:00 +0100

    Update env.ts

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,5 +1,5 @@
 const env = {
-  VITE_API_KEY: import.meta.env.VITE_API_KEY,
+  VITE_API_KEY: import.meta.env.VITE_API_KEY||46346179-9a76-494b-ac58-eac75b8327ec,
   VITE_API_URL:
     import.meta.env.VITE_API_URL || "https://nvc-api.onrender.com/users/",
 };
